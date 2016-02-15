482824a1c97d7655bae237d90b0d0d36d5d6f291
Chelladurai <pcdurai@live.com>
2016-02-15 18:00:41 +0200

    Initial version.

---
+++ Base64Conversion/Base64Conversion/ProgramBase64.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilityLocalLibrary;
+
+
+namespace Base64Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Base 64 testing 
+            Console.WriteLine("Start: Base 64 testing -------------------------------");
+            var inputString = "JkAT4OA3u9uJIe52ek7BobwBO:DzrxdQpIrSK7uBqsU1wHqXWBszymAq3LrFoqmtSjDI2MFzyj28";
+            var outputString1 = Base64Util.Base64Encode(inputString);
+            Console.WriteLine("Encoded string: " + outputString1);
+            Console.WriteLine("End: Base 64 testing -------------------------------");
+
+        }
+
+       
+
+
+    }
+}
---
+++ Base64Conversion/Base64LocalLibrary/DateTimeUtil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLocalLibrary
+{
+    class DateTimeUtil
+    {
+
+        /// <summary>
+        /// Generate the timestamp for the signature        
+        /// </summary>
+        /// <returns>A timestamp value in a string.</returns>
+        public static string GenerateTimeStamp()
+        {
+            // Default implementation of UNIX time of the current UTC time
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalSeconds, CultureInfo.CurrentCulture).ToString(CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// Generate a nonce
+        /// </summary>
+        /// <returns>A random number between 123400 and 9999999 in a string.</returns>
+        public static string GenerateNonce()
+        {
+            // Just a simple implementation of a random number between 123400 and 9999999
+            return new Random()
+                .Next(123400, int.MaxValue)
+                .ToString("X", CultureInfo.InvariantCulture);
+        }
+    }
+}
