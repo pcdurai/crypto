f4217b4bc8ebf3b95ec0985de8fcf81bd8193bce
pcdurai <pcdurai@live.com>
2016-02-15 18:18:25 +0200

    Fixed the error.

--- Base64Conversion/Base64LocalLibrary/DateTimeUtil.cs
+++ Base64Conversion/Base64LocalLibrary/DateTimeUtil.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UtilityLocalLibrary
 {
-    class DateTimeUtil
+    public class DateTimeUtil
     {
 
         /// <summary>
